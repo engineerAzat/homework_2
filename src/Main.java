e5f72fbd3ca3397a9a2bdb104105257495b889be
Azat <vildus@yandex.ru>
2024-04-19 21:13:41 +0300

    Домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -55,6 +55,79 @@ public class Main {
 
         // пятое задание
 
+        byte totalColors = 120;  // Необходимое количество банок краски для ремонта школы
+        byte witheColor = 2;  // Необходимое количество банок белой краски на один класс
+        byte brownColor = 4;  // Необходимое количество банок черной краски на один класс
+        byte totalClass = (byte) (totalColors / (witheColor + brownColor)); // Расчет общего количествл классов
+        byte totalWitheColors = (byte) (witheColor * totalClass); // Расчет необходимого количества банок белой краски
+        byte totalBrownColor = (byte) (brownColor * totalClass); // Расчет необходимого количество банок черной краски
+        System.out.println("В школе, где " + totalClass + " классов, нужно " + totalWitheColors + " банок белой краски и " + totalBrownColor + " банок коричневой краски");
+
+        // шестое задание
+
+        byte totalBananas = 5; // Общее количество бананов
+        byte weightBananas = 80; // Вес банана
+        short totalWeightBananas = (short) (totalBananas * weightBananas); // Общий вес бананов
+        short totalMilk = 200; // Общее количество молока
+        byte weightMilk = 105; // Вес молока в 100 граммах
+        short totalWeightMilk = (short) ((totalMilk / 100) * weightMilk); // Общий вес молока
+        byte totalIceCream = 2; // Общее количество мороженого
+        byte weightIceCream = 100; // Вес мороженого
+        short totalWeightIceCream = (short) (totalIceCream * weightIceCream); //  Общий вес мороженого
+        byte totalRawEgss = 4; // Общее количество сырых яиц
+        byte weightRawEggs = 70; // Вес сыроко яица
+        short totalWeightRawEggs = (short) (totalRawEgss * weightRawEggs); // Общий вес яиц
+        short totalWeightbreakfastGg = (short) (totalWeightBananas + totalWeightMilk + totalWeightIceCream + totalWeightRawEggs); // Вес спортивного завтрака в граммах
+        float totalWeightbreakfastKg = (float) (totalWeightBananas + totalWeightMilk + totalWeightIceCream + totalWeightRawEggs) / 1000; // Вес спорт завтрака в Килограммах
+
+        System.out.println(totalWeightbreakfastGg);
+        System.out.println(totalWeightbreakfastKg);
+
+        // Седьмое задание
+
+        byte excessWeightGr = 7; // Лишний вес спортсмена в кг
+        short excessWeightKg = (short) (excessWeightGr * 1000);  // Лишний вес спортсмена в гр
+        short minWeightOneDay = 250; // Минимальный вес который спортсмен может потерять в день в гр
+        short maxWeightOneDay = 500; // Максимальный вес который спортсмен может потерять в день в гр
+        byte maxTotalDays = (byte) (excessWeightKg / minWeightOneDay); // Максимальное количество дней для похудения
+        byte minTotalDays = (byte) (excessWeightKg / maxWeightOneDay); // Минимальное количество дней для похудения
+        byte averageDays = (byte) ((minTotalDays + maxTotalDays) / 2); // Среднее количество дней для похудения
+
+        System.out.println(maxTotalDays);
+        System.out.println(minTotalDays);
+        System.out.println(averageDays);
+
+        // Восьмое задание
+
+        int SalaryPerMonthMasha = 67760; // Зарплата Маши в месяц до повышения
+        int SalaryPerYearMasha = (SalaryPerMonthMasha * 12); // Годовой доход Маши до повышения
+        int SalaryPerYearMashaForPlusTenPercent = (int) (SalaryPerYearMasha + (SalaryPerYearMasha * 0.1)); // Годовой доход Маши после повышения на 10 процентов
+        int TotalSalaryMasha = (SalaryPerYearMashaForPlusTenPercent - SalaryPerYearMasha); // Разница годового дохода Маши после повышения на 10 процентов
+
+        int SalaryPerMonthDenis = 83690; // Зарплата Дениса в месяц до повышения
+        int SalaryPerYearDenis = (SalaryPerMonthDenis * 12); // Годовой доход Дениса до повышения
+        int SalaryPerYearDenisForPlusTenPercent = (int) (SalaryPerYearDenis + (SalaryPerYearDenis * 0.1)); // Годовой доход Дениса после повышения на 10 процентов
+        int TotalSalaryDenis = (SalaryPerYearDenisForPlusTenPercent - SalaryPerYearDenis); // Разница годового дохода Дениса после повышения на 10 процентов
+
+        int SalaryPerMonthKristina = 76230;  // Зарплата Кристины в месяц до повышения
+        int SalaryPerYearKristina = (SalaryPerMonthKristina * 12); // Годовой доход Кристины до повышения
+        int SalaryPerYearKristinaForPlusTenPercent = (int) (SalaryPerYearKristina + (SalaryPerYearKristina * 0.1)); // Годовой доход Кристины после повышения на 10 процентов
+        int TotalSalaryKristina = (SalaryPerYearKristinaForPlusTenPercent - SalaryPerYearKristina); // Разница годового дохода Кристины после повышения на 10 процентов
+
+        System.out.println("Маша теперь получает " + SalaryPerYearMashaForPlusTenPercent + " рублей. Годовой доход вырос на " + TotalSalaryMasha + " рублей");
+        System.out.println("Денис теперь получает " + SalaryPerYearDenisForPlusTenPercent + " рублей. Годовой доход вырос на " + TotalSalaryDenis + " рублей");
+        System.out.println("Кристина теперь получает " + SalaryPerYearKristinaForPlusTenPercent + " рублей. Годовой доход вырос на " + TotalSalaryKristina + " рублей");
+
+
+
+
+
+
+
+
+
+
+
 
 
 
